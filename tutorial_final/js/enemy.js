65b7b5a51d1ad9e23e332f7e3e0bce95c8d80797
Aaron McLeod <aaron.mcleod@mosaic.com>
2014-10-24 22:31:46 -0400

    swapped ship to use entity.

--- tutorial_final/js/enemy.js
+++ tutorial_final/js/enemy.js
@@ -1,14 +1,26 @@
-game.Enemy = me.Sprite.extend({
+game.Enemy = me.Entity.extend({
     init: function (x, y) {
-        this._super(me.Sprite, "init", [x, y, me.loader.getImage('ships'), 32, 32]);
-        this.body = new me.Body(this);
+        this._super(me.Entity, "init", [x, y, {
+            image: "ships",
+            width: 32,
+            height: 32
+        }]);
         this.chooseShipImage();
+        this.body.setVelocity(0, 0);
     },
 
     chooseShipImage: function () {
-        var xTile = ~~(Math.random() * 2) * this.width;
-        var yTile = ~~(Math.random() * 2) * this.height;
+        var frame = ~~(Math.random() * 3);
+        this.renderable.addAnimation("idle", [frame], 1);
+        this.renderable.setCurrentAnimation("idle");
+    },
 
-        this.offset.set(xTile, yTile);
+    update: function (time) {
+        this._super(me.Entity, "update", [time]);
+
+        this.body.update();
+
+
+        return true;
     }
 });
