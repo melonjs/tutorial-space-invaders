84458ec68b3d1f4b7872e990a7dea139cf0da2b2
Olivier Biot <olivier.biot@me.com>
2016-01-28 13:09:58 +0800

    melons 3.0 update

--- tutorial_step1/js/screens/play.js
+++ tutorial_step1/js/screens/play.js
@@ -3,9 +3,9 @@ game.PlayScreen = me.ScreenObject.extend({
      *  action to perform on state change
      */
     onResetEvent: function() {
-        me.game.world.addChild(new me.ColorLayer("background", "#000000", 0));
-        me.game.world.addChild(me.pool.pull("player"));
-        me.game.world.addChild(me.pool.pull("enemy", 50, 50));
+        me.game.world.addChild(new me.ColorLayer("background", "#000000"), 0);
+        me.game.world.addChild(me.pool.pull("player"), 1);
+        me.game.world.addChild(me.pool.pull("enemy", 50, 50), 2);
     },
 
 
