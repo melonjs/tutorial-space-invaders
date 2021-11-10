2fb1649df9ef637253c468bd295bc54e01ba551d
Kyle Keiper <kkeiper@starkstate.edu>
2021-11-10 13:58:33 -0500

    began retrofitting to es6 boilerplate. finished step1

---
+++ tutorial_step1/js/stage/play.js
@@ -0,0 +1,17 @@
+import * as me from 'https://cdn.jsdelivr.net/npm/melonjs@10.1.0/dist/melonjs.module.js';
+
+// Note : Jay Inheritance to be replaced with standard ES6 inheritance in melonjs 10+
+class PlayScreen extends me.Stage {
+    /**
+     *  action to perform on state change
+     */
+    onResetEvent() {
+
+        me.game.world.addChild(new me.ColorLayer("background", "#000000"), 0);
+
+        me.game.world.addChild(me.pool.pull("player"), 1);
+        me.game.world.addChild(me.pool.pull("enemy", 50, 50), 2);
+    }
+};
+
+export default PlayScreen;
